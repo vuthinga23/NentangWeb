8663884db76d8c29240afa548f1a7f2d6028cf3a
vuthinga23 <2251162083@e.tlu.edu.vn>
2025-07-29 00:25:56 +0700

    de 1

---
+++ de1/data.js
@@ -0,0 +1,33 @@
+const employeeData = [
+  {
+    name: "Thomas Hardy",
+    email: "thomashardy@mail.com",
+    address: "89 Chiaroscuro Rd, Portland, USA",
+    phone: "01715552222"
+  },
+  {
+    name: "Dominique Perrier",
+    email: "dominiqueperrier@mail.com",
+    address: "Obere Str. 57, Berlin, Germany",
+    phone: "03135555735"
+  },
+  {
+    name: "Maria Anders",
+    email: "mariaanders@mail.com",
+    address: "25, rue Lauriston, Paris, France",
+    phone: "05035559931"
+  },
+  {
+    name: "Fran Wilson",
+    email: "franwilson@mail.com",
+    address: "C/ Araquil, 67, Madrid, Spain",
+    phone: "02046195731"
+  },
+  {
+    name: "Martin Blank",
+    email: "martinblank@mail.com",
+    address: "Via Monte Bianco 34, Turin, Italy",
+    phone: "04806312097"
+  }
+];
+    
