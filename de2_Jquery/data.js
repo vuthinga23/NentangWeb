15cf5bc27f7a244ba2d12c8f21d687e60e7e4712
vuthinga23 <2251162083@e.tlu.edu.vn>
2025-08-03 17:18:46 +0700

    nop bai tap jquery

---
+++ de2_Jquery/data.js
@@ -0,0 +1,37 @@
+const data = [
+  {
+    id: 1101,
+    khachHang: "Nguyễn Văn A",
+    nhanVien: "Lê Thị B",
+    soTien: "150.000",
+    ngayMua: "06 Tháng 5 2024 9:45"
+  },
+  {
+    id: 1102,
+    khachHang: "Võ Hoài An",
+    nhanVien: "Mai Thục Anh",
+    soTien: "250.000",
+    ngayMua: "06 Tháng 5 2024 9:50"
+  },
+  {
+    id: 1103,
+    khachHang: "Hoàng Hà Thắng",
+    nhanVien: "Nguyễn Văn Hồng",
+    soTien: "330.000",
+    ngayMua: "06 Tháng 5 2024 9:52"
+  },
+  {
+    id: 1104,
+    khachHang: "Phạm Minh Quân",
+    nhanVien: "Trần Quốc Tuấn",
+    soTien: "180.000",
+    ngayMua: "06 Tháng 5 2024 10:00"
+  },
+  {
+    id: 1105,
+    khachHang: "Lê Thị Hạnh",
+    nhanVien: "Vũ Thị Hoa",
+    soTien: "420.000",
+    ngayMua: "06 Tháng 5 2024 10:05"
+  }
+];
---
+++ de3_Jquery/data.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    stt: 1,
+    ten: "Mai",
+    hoDem: "Thục Anh",
+    diaChi: "23 Hoàng Đạo Thúy, Thanh Xuân, Hà Nội",
+  },
+  {
+    stt: 2,
+    ten: "Hoàng",
+    hoDem: "Sinh Hồng",
+    diaChi: "23 Hoàng Đạo Thúy, Thanh Xuân, Hà Nội",
+  },
+  {
+    stt: 3,
+    ten: "Tính",
+    hoDem: "Mai Trung",
+    diaChi: "1411 Đường Láng, Cầu Giấy, Hà Nội",
+  },
+  {
+    stt: 4,
+    ten: "Hồng",
+    hoDem: "Nguyễn Văn",
+    diaChi: "1411 Đường Láng, Cầu Giấy, Hà Nội",
+  }
+];
